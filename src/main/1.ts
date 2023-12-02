6166389a419cdaebf18787e5793c7281ab56dedb
Lucas Wiese <lucas.wiese@gmx.de>
2023-12-02 01:13:33 +0100

    day 1 A2

--- src/main/1.ts
+++ src/main/1.ts
@@ -1,11 +1,31 @@
 import { reduce } from "lodash";
 import { read } from "../@util/read";
 
+const r = (x: string | undefined) =>
+  (x ?? "")
+    .replace(/one/g, "1")
+    .replace(/two/g, "2")
+    .replace(/three/g, "3")
+    .replace(/four/g, "4")
+    .replace(/five/g, "5")
+    .replace(/six/g, "6")
+    .replace(/seven/g, "7")
+    .replace(/eight/g, "8")
+    .replace(/nine/g, "9");
+const m = /\d|one|two|three|four|five|six|seven|eight|nine|/g;
+const mr = /\d|enin|thgie|neves|xis|evif|ruof|eerht|owt|eno|/g;
+
+const rev = (x: string) => x.split("").reverse().join("");
+
 const res = reduce(
   read(1).split(/\n/),
   (a, b) =>
-    a + +(b.split("").filter((x) => (x.match(/\d/) ?? []).length > 0)[0] +
-      b.split("").reverse().filter((x) => (x.match(/\d/) ?? []).length > 0)[0]),
+    a +
+    +(
+      r(b.match(m)?.filter(Boolean)[0]) +
+      r(rev(b).match(mr)?.filter(Boolean)[0])
+    ),
+
   0
 );
 
